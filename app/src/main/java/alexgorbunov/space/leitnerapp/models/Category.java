5d73fec4755876ce6cfd3ae1745f651facd80568
Alexander Gorbunov <alex.gorbunov.de@gmail.com>
2024-11-27 05:49:41 +0100

    added Category

---
+++ app/src/main/java/alexgorbunov/space/leitnerapp/models/Category.java
@@ -0,0 +1,11 @@
+package alexgorbunov.space.leitnerapp.models;
+
+public class Category {
+    private final int id;
+    private final String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
